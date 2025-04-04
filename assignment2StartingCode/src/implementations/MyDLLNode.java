6716868c6dd237ebf1e7f3d273e6533ea7a0680d
HamDan36 <150320417+HamDan36@users.noreply.github.com>
2025-04-04 13:14:52 -0600

    adds first half of the documentation for MyDLL

--- assignment2StartingCode/src/implementations/MyDLLNode.java
+++ assignment2StartingCode/src/implementations/MyDLLNode.java
@@ -9,16 +9,37 @@ public class MyDLLNode<E> {
     private MyDLLNode<E> next;
     private MyDLLNode<E> previous;
 
+    /**
+     * Constructor for a doubly linked list containing the specified data
+     * Preconditions: none
+     * Postconditions: a new MyDLL doubly linked list object is created containing the parameter data
+     * 
+     * @param data Data to be inserted into the new MyDLL object
+     */
     public MyDLLNode(E data) {
         this.data = data;
         this.next = null;
         this.previous = null;
     }
 
+    /**
+     * Returns the data from the node
+     * Preconditions: a valid MyDLL object must exist
+     * Postconditions: the data from the node is returned
+     * 
+     * @return Returns the data from the node
+     */
     public E getData() {
         return data;
     }
 
+    /**
+     * Sets the data into the node
+     * Preconditions: a valid MyDLL object must exist
+     * Postconditions: the data in the node is set to the parameter data
+     * 
+     * @param data Data to be placed into the node
+     */
     public void setData(E data) {
         this.data = data;
     }
